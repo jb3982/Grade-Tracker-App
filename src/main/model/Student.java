7c262f5541d41fb4af23f5b9a6a474fa436962b5
jaib <jbanyal@student.ubc.ca>
2024-03-27 17:55:44 -0700

    phase 3 commit 4

--- src/main/model/Student.java
+++ src/main/model/Student.java
@@ -73,10 +73,5 @@ public class Student {
         return json;
     }
 
-    @Override
-    public String toString() {
-        // Modify this to include the details you want to display about a Student
-        return "Student Name: " + this.getName() + ", ID: " + this.getStudentID();
-    }
 
 }
